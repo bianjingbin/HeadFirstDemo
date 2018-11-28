daf8e2339dc140d59a9636b4ceb7dcf8a41fd430
bianjingbin <771447518@qq.com>
2018-11-28 20:22:31 +0800

    day16(概念居多)

---
+++ src/main/java/com/javacore/HelloWorldTest.java
@@ -0,0 +1,21 @@
+/*
+ * ==============================================
+ * (C)2018 Shanghai KingstarWinning Corporation. All rights reserved.
+ * 项目名称： 规则引擎
+ * 系统名称： ENGINE3.0
+ * 文件名称： HelloWorldTest.java
+ * 注意事项：
+ * Id: HelloWorldTest.java,v3.0 2018年11月28日 bianjingbin Exp
+ * ==============================================
+ */
+package com.javacore;
+
+public class HelloWorldTest {
+
+	public static void main(String[] args) {
+		System.out.println("Java核心技术也将开始~~~");
+	}
+
+}
+
+/* Copyright (C) 2018, 上海金仕达卫宁软件科技有限公司 Project, All Rights Reserved. */
